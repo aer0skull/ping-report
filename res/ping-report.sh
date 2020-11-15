076459ba9d8c44dbb62efc9993e6cf190bc8dca5
Maxime Menault <maxime.menault@gmail.com>
2020-11-15 13:24:26 +0100

    Release 1.1.8 : Add output message for launch script

--- res/ping-report.sh
+++ res/ping-report.sh
@@ -4,9 +4,15 @@ BIN=/opt/ping-report/bin/ping-report
 PID=/var/log/ping-report/pid.log
 
 case $1 in
-start) sudo $BIN;;
-end) sudo kill `cat $PID`;sudo rm $PID;;
-restart) sudo kill `cat $PID`;sudo $BIN;;
-*) echo "Usage : ping-report [start | end | restart]";exit 1;;
+start)      sudo $BIN;
+            echo "ping-report started";;
+end)        sudo kill `cat $PID`;
+            sudo rm $PID;
+            echo "ping-report ended";;
+restart)    sudo kill `cat $PID`;
+            sudo $BIN;
+            echo "ping-report restarted";;
+*)          echo "Usage : ping-report [start | end | restart]";
+            exit 1;;
 esac
 
